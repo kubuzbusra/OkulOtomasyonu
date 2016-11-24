fcc9307e49aa87a776039e7a8fad3190feb81c85
kubuzbusra <kubuzbusra@gmail.com>
2016-11-24 13:31:41 +0300

    added project

--- Program.cs
+++ Program.cs
@@ -13,6 +13,7 @@ namespace Okul.WFA
         /// </summary>
         [STAThread]
         static void Main()
+
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
